b48a813ee1ea0c1b548e9ee79ad64a10ad256538
tien2032002 <tien.nguyentien203@hcmut.edu.vn>
2022-12-13 10:48:27 +0700

    add init state, fix some bug, add pwm

--- final_project_test/Core/Inc/hardware_layer.h
+++ final_project_test/Core/Inc/hardware_layer.h
@@ -8,6 +8,10 @@
 #ifndef INC_HARDWARE_LAYER_H_
 #define INC_HARDWARE_LAYER_H_
 
+#define DO		1
+#define VANG	2
+#define XANH	3
+
 void set_color_light1(int color);
 
 void set_color_light2(int color);
